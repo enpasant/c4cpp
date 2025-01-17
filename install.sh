f345b71a2f62052ad3cf8982ad2dc948ec386b9a
enpasant <163363163+enpasant@users.noreply.github.com>
2025-01-17 19:58:44 -0400

    Install script

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+if [ -e "/usr/bin/c4cpp" ]; then
+	stype="rmc"
+	echo "Would you like to remove c4cpp? (y/n) "
+	read choicerm
+	if [ "$choicerm" = "y" ]; then
+		sudo rm /usr/bin/c4cpp
+		echo "Done."
+	else
+		echo "Abort."
+	fi
+fi
+
+if [ ! -e "/usr/bin/c4cpp" ]; then
+	if [ "$stype" = "rmc" ]; then
+		echo "Script ended"
+
+	else 
+		echo "Would you like to extract c4cpp /usr/bin/? (y/n)"
+		read choiceex
+		if [ "$choiceex" = "y" ]; then
+			echo "Building c4cpp..."
+			g++ main.cpp -o c4cpp
+			sudo mv c4cpp /usr/bin/
+			echo "Compiled and extracted."
+		else
+			echo "Abort."
+		fi
+	fi
+fi
+
